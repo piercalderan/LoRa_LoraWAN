ac98e7f58a25fd88a2f65dbc90e22c7d1bc92b5a
Pier Calderan <pier@calderan.info>
2023-11-20 10:34:52 +0200

    Delete Progetto Gateway directory

--- Progetto Gateway/MULTI_CHAN/reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 17 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio17/direction
-echo 1 > /sys/class/gpio/gpio17/value
-sleep 5
-echo 0 > /sys/class/gpio/gpio17/value
-sleep 1
-echo 17 > /sys/class/gpio/unexport
-echo “reset complete!”
